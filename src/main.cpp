9373a6756a688269eb334d1f746336112f4fb5e0
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2026-01-19 11:23:57 +0530

    REPL loop

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,21 @@ int main() {
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // TODO: Uncomment the code below to pass the first stage
-  std::cout << "$ ";
+  // REPL
+  /*
+    Read :  Display a prompt and wait for user input
+    Eval : Parse and execute the command
+    Print : Display the output or error message
+    Loop : Return to step 1 and wait for the next command
+  */
+  while(true){
+    cout << "$ ";
+    // Capture user's commands
+    string command;
+    getline(cin, command);
 
-  // Capture user's commands
-  string command;
-  getline(cin, command);
-
-  // Command not found message
-  cout<<command<<": command not found "<<endl;
+    // Command not found message
+    cout<<command<<": command not found "<<endl;
+  }
 
 }
