bc25c9aa4134dad2e40c5b13eaa1b9afddfcca43
abhishektiwari283 <142007480+Code-writter@users.noreply.github.com>
2026-01-19 11:15:25 +0530

    Getting user commands and error

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -8,4 +9,12 @@ int main() {
 
   // TODO: Uncomment the code below to pass the first stage
   std::cout << "$ ";
+
+  // Capture user's commands
+  string command;
+  getline(cin, command);
+
+  // Command not found message
+  cout<<command<<" : command not found "<<endl;
+
 }
